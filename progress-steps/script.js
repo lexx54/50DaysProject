32825626295e33c9f0f2c5aa2f39e764ab652f24
lexx54 <chapelexx54@gmail.com>
2021-07-11 10:51:57 -0400

    finishes progress step bar

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,17 +1,45 @@
 const d = document;
 
-const buttons = d.getElementsByTagName("button");
+const [prevBtn,nextBtn] = d.getElementsByTagName("button");
+const steps = d.getElementsByClassName("is-selected");
 
-console.log(buttons)
 
+prevBtn.addEventListener("click", () => {
+  if(steps.length<2){
+    prevBtn.classList.add("is-disabled")
+  } else {
+    const prevLine = steps[steps.length-1].previousElementSibling;
+    const prevMark = prevLine.nextElementSibling;
+    
+    nextBtn.classList.remove("is-disabled");
+    nextBtn.disabled = false;
 
-for( let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click",e=>{
-    if (e.target.id === "prev"){
-      console.log("para atras")
-    } else if (e.target.id ==="next"){
-      console.log("para adelante")
+    prevLine.classList.remove("is-selected");
+    prevMark.classList.remove("is-selected");
+
+    if(steps.length===1) {
+      prevBtn.classList.add("is-disabled");
+      prevBtn.disabled = true;
     }
-  })
+  }
+})
 
-}
+nextBtn.addEventListener("click", () =>{
+  if(steps.length>6){
+    nextBtn.classList.add("is-disabled");
+  } else{
+    const nextLine = steps[steps.length-1].nextElementSibling;
+    const nextMark = nextLine.nextElementSibling;
+
+    nextLine.classList.add("is-selected");
+    nextMark.classList.add("is-selected");
+
+    prevBtn.classList.remove('is-disabled');
+    prevBtn.disabled = false;
+
+    if(steps.length===7) {
+      nextBtn.classList.add("is-disabled");
+      nextBtn.disabled = true;
+    };
+  }
+})
