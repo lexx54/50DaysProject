4f30231893839a4a651d2482a51250d72a98dccc
lexx54 <chapelexx54@gmail.com>
2021-07-17 18:47:17 -0400

    Finishes Rotating nav project

---
+++ rotating-nav/script.js
@@ -0,0 +1,24 @@
+const menu = document.getElementsByClassName("menu")[0];
+const main = document.getElementsByTagName("main")[0];
+const nav = document.getElementsByTagName("nav")[0];
+const navChildren = nav.children;
+console.log(navChildren)
+document.addEventListener("click", e => {
+  if(e.target.nodeName === "I"){
+    // console.log(e);
+    if(e.target.classList[1] === "fa-bars"){
+      menu.classList.add("rotate");
+      main.classList.add("rotate-container");
+      for(let i=0; i < 3; i++){
+        navChildren[i].classList.add("translate");
+      }
+    }else{
+      menu.classList.remove('rotate');
+      main.classList.remove("rotate-container");
+      for(let i=0; i < 3; i++){
+        navChildren[i].classList.remove("translate");
+      }
+    }
+  }
+ 
+})
